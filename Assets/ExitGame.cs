3fc4e82ef5685cfdfcf921f002993f2624de7f14
trunganh <trantrunganh04032003@gmial.com>
2023-12-06 17:24:21 +0700

    init comit

---
+++ Assets/ExitGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Exit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Exit();
+    }
+
+    public void Exit()
+    {
+        if(this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime > 3)
+        {
+            Debug.Log("Exit");
+            Application.Quit();
+        }
+    }
+}
