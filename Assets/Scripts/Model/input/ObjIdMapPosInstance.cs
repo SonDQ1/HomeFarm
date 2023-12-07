ec1add28baa71eb381dfeeb2e37080a6464e4c65
trunganh <trantrunganh04032003@gmial.com>
2023-12-07 15:22:50 +0700

    delete

--- Assets/Scripts/Model/input/ObjIdMapPosInstance.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjIdMapPosInstance : MonoBehaviour {
-
-    public string objtag;
-    public int id;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# HomeFarm
-HomeFarm
